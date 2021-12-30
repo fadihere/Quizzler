bb78be9819c1ce88bb2241d1a2156a917b77251d
fadihere <fadihere@outlook.com>
2021-12-30 11:23:14 +0500

    Add Pop Up Package & Restart the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quizbrain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -38,6 +38,16 @@ class _QuizPageState extends State<QuizPage> {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
+      if (quizBrain.isFinished()) {
+        Alert(
+                context: context,
+                title: "Congratulations!",
+                desc: "You Successfully completed the Quiz")
+            .show();
+
+        quizBrain.reset();
+        scoreKeeper = [];
+      }
       //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
       //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
       //HINT! Step 4 Part B is in the quiz_brain.dart
@@ -45,18 +55,20 @@ class _QuizPageState extends State<QuizPage> {
       //TODO: Step 4 Part D - empty out the scoreKeeper.
 
       //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
-      if (userPickedAnswer == correctAnswer) {
-        scoreKeeper.add(Icon(
-          Icons.check,
-          color: Colors.green,
-        ));
-      } else {
-        scoreKeeper.add(Icon(
-          Icons.close,
-          color: Colors.red,
-        ));
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
       }
-      quizBrain.nextQuestion();
     });
   }
 
@@ -128,9 +140,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
